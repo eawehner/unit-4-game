9390d02b943869215db4d1043125a1cfee73c33e
EAWehner <elizabethawehner@gmail.com>
2018-10-01 21:45:01 -0400

    mgame is in a playable state now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,15 +10,17 @@ var materias = [
 
 var valueArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-$(document).ready(function() {
+var wins = 0;
 
-    var randomNum = Math.floor(Math.random() * (121+1) + 19);
+var losses = 0;
 
-    var scoreCount = 0;
+var currentCount = 0;
 
-    $("#goal").text(randomNum);
+function gameStart() {
 
-    $("#score").text(scoreCount);
+    var currentCount = 0;
+
+    $("#current").text(currentCount);
 
     // displaying the materia images
     for (i=0; i < 4; i++) {
@@ -28,15 +30,13 @@ $(document).ready(function() {
 
         var materiaValue = valueArray[randomValue];
 
-        // var materiaValue = Math.floor(Math.random() * (12*1) + 1);
-
         var displayedMateria = $("<img>");
 
         displayedMateria.addClass("materiaImg");
 
-        displayedMateria.attr("src", materias[randomMateria]);
+        displayedMateria.attr("src", materias[randomMateria]).attr("data-materiaValue", materiaValue);
 
-        displayedMateria.attr("data-materiaValue", materiaValue);
+        // displayedMateria.attr("data-materiaValue", materiaValue);
 
         $("#materiaBox").append(displayedMateria);
 
@@ -45,14 +45,84 @@ $(document).ready(function() {
 
         valueArray.splice(randomValue, 1);
     };
+};
 
+function restart() {
+    // refills the array with images to start over again
+    materias = [
+        "./assets/images/bluemateria.png",
+        "./assets/images/greenmateria.png",
+        "./assets/images/purplemateria.png",
+        "./assets/images/redmateria.png",
+        "./assets/images/tealmateria.png",
+        "./assets/images/whitemateria.png",
+        "./assets/images/yellowmateria.png"
+    ];
+    
+    // and numbers
+    var valueArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    var currentCount = 0;
+
+    var randomNum = Math.floor(Math.random() * (121+1) + 19);
+
+    $("#goal").text(randomNum);
+
+    $("#materiaBox").empty();
+
+    gameStart(materias, valueArray, currentCount);
+
+    // when materia is clicked
     $(".materiaImg").on("click", function() {
         var materiaNum = ($(this).attr("data-materiaValue"));
         materiaNum = parseInt(materiaNum);
     
-        scoreCount += materiaNum;
+        currentCount += materiaNum;
     
-        $("#score").text(scoreCount);
+        $("#current").text(currentCount);
+
+        // checking for wins and losses
+        if (currentCount === randomNum) {
+            wins++;
+            $("#winNum").text(wins);
+            restart();
+        } else if (currentCount >= randomNum) {
+            losses++;
+            $("#lossesNum").text(losses);
+            restart();
+        };
+
     });
+};
+
+$(document).ready(function() {
+
+    // var randomNum = Math.floor(Math.random() * (121+1) + 19);
+
+    // $("#goal").text(randomNum);
+
+    restart();
+
+    // gameStart(materias, valueArray, randomNum);
+
+    // $(".materiaImg").on("click", function(randomNum) {
+    //     var materiaNum = ($(this).attr("data-materiaValue"));
+    //     materiaNum = parseInt(materiaNum);
+    
+    //     currentCount += materiaNum;
+    
+    //     $("#current").text(currentCount);
+
+    //     if (currentCount === randomNum) {
+    //         wins++;
+    //         $("#winNum").text(wins);
+    //         restart();
+    //     } else if (currentCount >= randomNum) {
+    //         losses++;
+    //         $("#lossesNum").text(losses);
+    //         restart();
+    //     };
+
+    // });
 
 });
